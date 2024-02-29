110ee6e6952c3eaf556052a4956c3253931082f9
Rafael Patrick de Souza <rafael.souza@navarraaluminio.com>
2024-02-29 18:39:24 +0000

    New Components

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <v-app>
     <!-- NavBar Superior -->
-    <v-app-bar app :color="colors.primary" dark>
+    <v-app-bar app :color="colors.secondary" dark>
       <v-toolbar-title>PeregrinAr-Te</v-toolbar-title>
     </v-app-bar>
 
@@ -12,9 +12,17 @@
         <v-row justify="center">
           <v-col cols="12" sm="8" md="6">
             <h1 class="text-h4 text-center" :style="{ color: colors.secondary }">Bem-vindo ao PeregrinAr-Te</h1>
+
+            <v-divider class="my-3"></v-divider>
+            <div class="blue">
+              <p class="text-center" :style="{ color: colors.neutralLight}">
+                Este é um projeto de arte e cultura que visa promover a arte e a cultura em geral, com foco na arte sacra
+                e na peregrinação.
+              </p>
+            </div>
           </v-col>
         </v-row>
-        
+
         <!-- Aqui vai o restante do seu conteúdo -->
       </v-container>
     </v-main>
@@ -28,7 +36,7 @@ export default {
   name: 'App',
   setup() {
     const colors = ref({
-      primary: '#6c7540',
+      primary: '#124c74',
       secondary: '#422e00',
       tertiary: '#6c7540',
       accent: '#ecd5af',
@@ -48,8 +56,33 @@ export default {
 }
 
 .v-main {
-  /* Estilo personalizado para o conteúdo principal */
+  background-image: url(https://img.freepik.com/fotos-premium/fundo-de-textura-de-papel-retro-vintage-velho-grunge_47840-948.jpg?size=626&ext=jpg&ga=GA1.1.87170709.1707782400&semt=ais);
+  background-repeat: repeat;
+  background-position: center;
 }
 
-/* Você pode adicionar mais estilos conforme necessário */
+body * {
+  /* font-family: 'Merriweather', serif; */
+  font-family: 'Roboto Slab', serif;
+}
+
+.blue {
+  color: #6c7540;
+  background-color: #124c74;
+  padding: 4rem;
+  border-radius: 2rem;
+}
+
+.text-h4 {
+  font-size: 2.5rem;
+  font-weight: 500;
+  padding: 2rem 0;
+}
+
+
+p {
+  font-size: 1.5rem;
+  font-weight: 300;
+  padding: 1rem 0;
+}
 </style>
