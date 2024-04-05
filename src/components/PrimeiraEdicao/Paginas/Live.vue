db12c316e6055534098764ad994b3e38131db051
Rafael Patrick de Souza <rafael.souza@navarraaluminio.com>
2024-04-05 09:25:34 +0100

    Atualizações na exibição da página Live.vue

--- src/components/PrimeiraEdicao/Paginas/Live.vue
+++ src/components/PrimeiraEdicao/Paginas/Live.vue
@@ -1,34 +1,36 @@
 <template>
     <div class="video-container">
-      <iframe
-        width="560"
-        height="315"
-        :src="videoUrl"
-        title="YouTube video player"
-        frameborder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-        allowfullscreen>
-      </iframe>
+        <h1>PeregrinAr-Te - Live</h1>
+        <iframe width="560" height="315" :src="videoUrl" title="YouTube video player" frameborder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowfullscreen>
+        </iframe>
+
+      
+        <p class="mensagem-aguarde">
+            Por favor, aguarde a transmissão começar. Agradecemos a sua paciência.
+        </p>
     </div>
-  </template>
-  
-  <script setup>
-  import { ref } from 'vue';
-  
-  const videoID = 'AQUI_VAI_O_ID_DO_SEU_VIDEO';
-  const videoUrl = ref(`https://www.youtube.com/embed/${videoID}?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=1`);
-  </script>
-  
-  <style>
-  .video-container {
+</template>
+
+<script setup>
+import { ref } from 'vue';
+
+const videoID = 'AQUI_VAI_O_ID_DO_SEU_VIDEO';
+const videoUrl = ref(`https://www.youtube.com/embed/${videoID}?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=1`);
+</script>
+
+<style>
+.video-container {
     display: flex;
     justify-content: center;
     align-items: center;
-    height: 100vh; /* Ajusta a altura para ocupar toda a altura da tela */
-  }
-  
-  iframe {
+    height: 100vh;
+    /* Ajusta a altura para ocupar toda a altura da tela */
+}
+
+iframe {
     /* Ajustes adicionais para responsividade podem ser adicionados aqui */
-    box-shadow: 0 0 8px rgba(0,0,0,0.5);
-  }
-  </style>
+    box-shadow: 0 0 8px rgba(0, 0, 0, 0.5);
+}
+</style>
