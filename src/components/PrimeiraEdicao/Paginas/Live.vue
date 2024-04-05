ccdd43d32c58c71b528ad27e8f54adbabfd7476c
Rafael Patrick de Souza <rafael.souza@navarraaluminio.com>
2024-04-05 11:38:31 +0100

    Atualizações na exibição da página Live.vue e adição de quebra de linha

--- src/components/PrimeiraEdicao/Paginas/Live.vue
+++ src/components/PrimeiraEdicao/Paginas/Live.vue
@@ -3,14 +3,24 @@
     <div class="video-container">
         <h1>PeregrinAr-Te - Live</h1>
         <br>
-        <iframe width="760" height="515" :src="videoUrl" title="YouTube video player" frameborder="0"
-            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen>
-        </iframe>
+        <div class="d-flex flex-row justify-space-around">
+            <div class="w-25"></div>
+            <iframe width="760" height="515" :src="videoUrl" title="YouTube video player" frameborder="0"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                allowfullscreen>
+            </iframe>
+            <div class="w-25 pl-8  text-center d-flex flex-column align-center justify-center ">
+                <a href="https://www.youtube.com/channel/UCwA0F52Y-mnFu9QP0yVN-Lg" target="_blank">
+                    <img src="@/assets/youtube.png" alt="Logo do canal PeregrinAr-Te" width="80px" />
+                    <p>Inscreva-se no nosso canal do youtube</p>
+                </a>
+            </div>
+        </div>
 
-      <br>
+        <br>
         <p class="mensagem-aguarde">
             Por favor, aguarde a transmissão começar. Agradecemos a sua paciência.
+            <v-img src="@/assets/plateia.svg" id="plateia"> </v-img>
         </p>
     </div>
 </template>
@@ -28,7 +38,7 @@ function onItemClick(tab) {
 }
 
 const videoID = 'AQUI_VAI_O_ID_DO_SEU_VIDEO';
-const videoUrl = ref('https://www.youtube.com/embed/'+videoID+'?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=1');
+const videoUrl = ref('https://www.youtube.com/embed/' + videoID + '?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=1');
 </script>
 
 <style>
