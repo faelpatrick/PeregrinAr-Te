ec32b4acef71a7375180eaf5254e728cf8a99204
Rafael Patrick de Souza <45822152+faelpatrick@users.noreply.github.com>
2024-04-06 13:42:21 +0100

    Update Live.vue

--- src/components/PrimeiraEdicao/Paginas/Live.vue
+++ src/components/PrimeiraEdicao/Paginas/Live.vue
@@ -5,7 +5,7 @@
         <br>
         <div class="d-flex flex-row justify-space-around">
             <div class="w-25"></div>
-            <iframe width="760" height="515" :src="videoUrl" title="YouTube video player" frameborder="0"
+            <iframe  height="600" :src="videoUrl" title="YouTube video player" frameborder="0"
                 allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
                 allowfullscreen>
             </iframe>
