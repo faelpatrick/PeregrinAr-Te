087d59082e6cfa4f52cc4edf5720a6e7eae77a06
Rafael Patrick de Souza <45822152+faelpatrick@users.noreply.github.com>
2024-04-18 17:40:17 +0100

    .

--- src/components/PrimeiraEdicao/Paginas/Live.vue
+++ src/components/PrimeiraEdicao/Paginas/Live.vue
@@ -37,7 +37,7 @@ function onItemClick(tab) {
   props.activeTab.value = tab;
 }
 
-const videoID = '8mAN-wH3o_o';
+const videoID = 'hUeizfJawnY';
 const videoUrl = ref('https://www.youtube.com/embed/' + videoID + '?rel=0&modestbranding=1&autohide=1&showinfo=0&controls=1');
 </script>
 
